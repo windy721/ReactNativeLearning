8b7224a08e2652417cd42f51bfaf820ff7821409
JimKong <105442529@qq.com>
2016-09-01 11:46:30 +0800

    [add][what]Add the event react from Native code to the JS code

--- JimTextView.js
+++ JimTextView.js
@@ -1,5 +1,5 @@
-var {requireNativeComponent, View} = require('react-native');
-var {PropTypes} = require('react');
+import React, { Component, PropTypes } from 'react';
+var {requireNativeComponent, View, ToastAndroid} = require('react-native');
 
 var myTextView = {
     name: 'MyTextView',
@@ -11,4 +11,29 @@ var myTextView = {
         ...View.propTypes // include the default view properties
     }
 }
-module.exports = requireNativeComponent('JimTextView', myTextView);
+var RCTMyView = requireNativeComponent('JimTextView', myTextView);
+
+class MyJimTextView extends Component {
+    constructor(props) {
+        super(props);
+        this._onChange = this._onChange.bind(this);
+    }
+
+    _onChange(event:Event) {
+        if (!this.props.onChangeMessage) {
+            return;
+        }
+        if (event.nativeEvent.message === 'MyMessage') {
+            this.props.onChangeMessage();
+        }
+    }
+
+    render() {
+        return <RCTMyView {...this.props} onChange={this._onChange}/>;
+    }
+}
+
+MyJimTextView.propTypes = {
+    onChangeMessage: PropTypes.func,
+}
+module.exports = MyJimTextView;
--- android/app/src/main/java/com/awesomeproject/views/JimTextViewManager.java
+++ android/app/src/main/java/com/awesomeproject/views/JimTextViewManager.java
@@ -1,11 +1,17 @@
 package com.awesomeproject.views;
 
 import android.graphics.Color;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.annotations.ReactProp;
+import com.facebook.react.uimanager.events.RCTEventEmitter;
 
 /**
  * Created by JimKong on 2016/8/31.
@@ -17,28 +23,46 @@ public class JimTextViewManager extends SimpleViewManager<TextView> {
     }
 
     @Override
-    protected TextView createViewInstance(ThemedReactContext reactContext) {
-        TextView textView = new TextView(reactContext);
+    protected TextView createViewInstance(final ThemedReactContext reactContext) {
+        final TextView textView = new TextView(reactContext);
+        textView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    WritableMap nativeEvent = Arguments.createMap();
+                    nativeEvent.putString("message", "MyMessage");
+                    reactContext.getJSModule(RCTEventEmitter.class).receiveEvent(
+                            textView.getId(), "topChange", nativeEvent
+                    );
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
         return textView;
     }
 
-    @ReactProp(name="text")
-    public void setText(TextView view,String text){
-        view.setText(text);
+    @ReactProp(name = "text")
+    public void setText(TextView view, String text) {
+        view.setText("HAHA: " + text);
     }
-    @ReactProp(name="textSize")
-    public void setTextSize(TextView view,float fontSize){
+
+    @ReactProp(name = "textSize")
+    public void setTextSize(TextView view, float fontSize) {
         view.setTextSize(fontSize);
     }
-    @ReactProp(name="textColor",defaultInt = Color.BLACK)
-    public void setTextColor(TextView view,int textColor){
+
+    @ReactProp(name = "textColor", defaultInt = Color.BLACK)
+    public void setTextColor(TextView view, int textColor) {
         view.setTextColor(textColor);
     }
-    @ReactProp(name="isAlpha",defaultBoolean = false)
-    public void setTextAlpha(TextView view,boolean isAlpha){
-        if(isAlpha){
+
+    @ReactProp(name = "isAlpha", defaultBoolean = false)
+    public void setTextAlpha(TextView view, boolean isAlpha) {
+        if (isAlpha) {
             view.setAlpha(0.5f);
-        }else{
+        } else {
             view.setAlpha(1.0f);
         }
     }
